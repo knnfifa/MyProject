6007fbb4db663b2d0ef0a42603fed813f29af5fe
mart <kunanon.su@ku.th>
2025-02-18 21:42:40 +0700

    set mvn

--- myproject/src/main/java/com/project/App.java
+++ myproject/src/main/java/com/project/App.java
@@ -1,10 +1,66 @@
 package com.project;
-
-/**
- * Hello world!
- */
-public class App {
+ 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.geometry.*;
+ 
+public class App extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+    
+        TextField firstNumberField = new TextField("0");
+        TextField secondNumberField = new TextField("0");
+
+    
+        firstNumberField.setPrefColumnCount(4);
+        secondNumberField.setPrefColumnCount(4);
+
+    
+        Label plusLabel = new Label("+");
+        Label equalsLabel = new Label("=");
+
+    
+        Button addButton = new Button("Add");
+
+    
+        Label resultLabel = new Label("0");
+
+    
+        addButton.setOnAction(e -> {
+            try {
+            
+                int num1 = Integer.parseInt(firstNumberField.getText());
+                int num2 = Integer.parseInt(secondNumberField.getText());
+                int result = num1 + num2;
+                resultLabel.setText(String.valueOf(result));
+            } catch (NumberFormatException ex) {
+                resultLabel.setText("Invalid input");
+            }
+        });
+
+    
+        HBox topRow = new HBox(10, firstNumberField, plusLabel, secondNumberField, equalsLabel, resultLabel);
+        topRow.setAlignment(Pos.CENTER);
+
+    
+        VBox content = new VBox(20, topRow, addButton);
+        content.setAlignment(Pos.CENTER);
+        content.setPadding(new Insets(20));
+
+    
+        Scene scene = new Scene(content, 500, 200);
+
+    
+        stage.setScene(scene);
+        stage.setTitle("Adder");
+        stage.setResizable(false);
+        stage.show();
     }
 }
